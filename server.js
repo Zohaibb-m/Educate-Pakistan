532e9b9c59c2c834a9033b7cff9d7c57644b2677
Zohaib Munir <zohaibmunir32@gmail.com>
2022-04-30 19:52:48 +0500

    Initial Commit

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ mongoose.connect(URI,{
     console.log("Connected to Mongo DB");
 }) 
 
-app.use(express.static(path.join(__dirname, './client/build')))
-console.log(__dirname, './client/build')
+app.use(express.static(path.join(__dirname, '/client/build')))
+console.log(__dirname, '/client/build')
 
 app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
